20010bd2a91ea5c90a11b05d75f758c6c412d7ec
Zarbul <dlyapicem@gmail.com>
2018-01-22 15:48:30 +0300

    formatirovanie

--- lesson_1/Task_2.py
+++ lesson_1/Task_2.py
@@ -1,4 +1,4 @@
 print("Введите числа которые нужно сложить:")
-num1=int(input("->"))
-num2=int(input("->"))
+num1 = int(input("->"))
+num2 = int(input("->"))
 print("Сумма этих чисел равна", num1+num2)
