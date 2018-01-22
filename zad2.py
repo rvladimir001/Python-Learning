8ccb144000ca476765753aa4d7edb2fc89814145
Zarbul <dlyapicem@gmail.com>
2018-01-22 14:15:50 +0300

    first commit

--- zad2.py
+++ zad2.py
@@ -1,4 +1,5 @@
 #-*-coding UTF-8-*-
 print("Введите числа которые нужно сложить:")
-num1, num2 =int(input("->")), int(input("->"))
+num1=int(input("->"))
+num2=int(input("->"))
 print("Сумма этих чисел равна", num1+num2)
