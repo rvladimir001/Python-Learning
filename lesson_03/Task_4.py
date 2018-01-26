ff10cb833c2948be9b27b888fbb6d88aaf89e114
Zarbul <dlyapicem@gmail.com>
2018-01-26 15:20:55 +0300

    немного упростил

--- lesson_03/Task_4.py
+++ lesson_03/Task_4.py
@@ -7,12 +7,9 @@
   Написать программу, определяющую значение y по введеному значению x.
 """
 x = float(input("Введите Х ->"))
+y = -x
 if x > 10:
     y = x + 1.5
-    print("y=f(x) >>> y={}".format(y))
 elif x == 10 or x == 0:
     y = 0
-    print("y=f(x) >>> y={}".format(y))
-elif x < 10:
-    y = -x
-    print("y=f(x) >>> y={}".format(y))
+print("y=f(x) >>> y={}".format(y))
