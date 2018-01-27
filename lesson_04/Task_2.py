97a37ff6df1ca68353be3d6a4f08c6a353c02b81
Zarbul <dlyapicem@gmail.com>
2018-01-27 21:54:26 +0300

    доделал

--- lesson_04/Task_2.py
+++ lesson_04/Task_2.py
@@ -4,7 +4,7 @@
 Вывести на экран таблицу умножения на 3.
 """
 
-for i in range(1, 4):
+for i in range(1, 10):
     for j in range(1, 4):
-        print(i * j, end=" ")
+        print(i * j, end="\t")
     print("\n")
