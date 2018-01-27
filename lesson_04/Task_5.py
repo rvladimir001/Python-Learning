7139479f70b08508dc8ddfe67db077034d3d0ae7
Zarbul <dlyapicem@gmail.com>
2018-01-28 00:29:57 +0300

    edit

--- lesson_04/Task_5.py
+++ lesson_04/Task_5.py
@@ -11,3 +11,13 @@ while num > 0:
         KakZnayuTakPishu += 1
     num = num // 10
 print("В числе {} чентых цифр".format(KakZnayuTakPishu))
+
+print("~" * 50)
+
+num = input("num=")
+fignya = 0
+for i in num:
+    if int(num) % 2 == 0:
+        fignya +=1
+    num = int(num) //10
+print(fignya)
