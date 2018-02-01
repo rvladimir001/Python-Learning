12c3a091e19ad30693329929ab69b94904f03f4b
Zarbul <dlyapicem@gmail.com>
2018-02-01 16:31:11 +0300

    first

--- lesson_06/Task_4.py
+++ lesson_06/Task_4.py
@@ -6,6 +6,7 @@
 варианта вычисления максимального элемента: с приминением цикла и без него.
 """
 #first variant
+#наполняем список рандомными числами
 import random
 
 lst = []
@@ -14,9 +15,43 @@ while i < 6:
     lst.append(random.randint(0, 100))
     i += 1
 print(lst)
+
 lst.sort(reverse=True)
 print(lst)
+
 print(lst[0])
 
+print("~" * 50)
 
 #second variant
+#наполняем список рандомными числами
+import random
+
+lst = []
+i = 0
+while i < 6:
+    lst.append(random.randint(0, 100))
+    i += 1
+print(lst)
+
+print(max(lst))
+
+print("~" * 50)
+
+#
+#наполняем список рандомными числами
+import random
+
+lst = []
+i = 0
+while i < 6:
+    lst.append(random.randint(0, 100))
+    i += 1
+print(lst)
+
+num = lst[0]
+for i in lst:
+    if i > num:
+        num = i
+        i +=1
+print(num)
