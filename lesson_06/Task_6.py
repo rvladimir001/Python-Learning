f2c37160848ea6ea5b93fb42712a6be961d9fc7c
Zarbul <dlyapicem@gmail.com>
2018-02-02 14:37:52 +0300

    загвоздка во второй части с использованием sqrt

--- lesson_06/Task_6.py
+++ lesson_06/Task_6.py
@@ -4,14 +4,35 @@
 Заполнить список из шести элементов квадратными корнями произвольныз
 целочисленныз значений. Вывести список на экран через запятую.
 """
+#********************************************************
 import random
 
-lst = []
-i = 0
-while i < 6:
-    lst.append(random.randint(0, 100))
-    i += 1
-print(lst)
+count = 6
+
+lst = [random.randint(0, 100) for i in range(count)]
+print("lst=", lst)
+
+print("~" * 50)
+
 for j in lst:
     j = j ** 0.5
     print(j, end=", ")
+#********************************************************
+
+print("*" * 50)
+
+import random
+import math
+
+count = 6
+"""randoms = [str(math.sqrt(random.randint(1, 101))) for _ in range(count)]
+print('Исходный список ', randoms)
+"""
+
+lst = [random.randint(0, 100) for i in range(count)]
+print("lst=", lst)
+lst1 = [str(math.sqrt(lst))]
+#********************************************************
+print("~" * 50)
+
+#lst1 = str(math.sqrt(lst))
