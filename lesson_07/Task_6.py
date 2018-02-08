e038b0a6242cf49980a34002ec242992a3ad8ab4
Zarbul <dlyapicem@gmail.com>
2018-02-08 16:32:54 +0300

    добавил чужой код

--- lesson_07/Task_6.py
+++ lesson_07/Task_6.py
@@ -1,6 +1,37 @@
 """
 Задача 6
-Работаем с чужим кодом. В коде из файла https://github.com/groall/python_learning/blob/master/lesson_04_cycles/task03.py
+Работаем с чужим кодом. В коде из файла
+https://github.com/groall/python_learning/blob/master/lesson_04_cycles/task03.py
 нужно заменить повторяющееся с циклах действие суммирования на вызов функции,
 функция должна работать с глобальной переменной sumNumbers.
+"""
+
+"""
+# Пользователь вводит любое целое положительное число (сделать проверку).
+# Программа должна просуммировать все числа от 1 до введенного пользователем числа (не включая его).
+
+x = input('Введите целое положительное число: ')
+
+if not x.isdigit():
+    print("Вы ввели не число")
+    exit()
+
+xInt = int(x)
+if xInt < 1:
+    print("Число не положительное")
+    exit()
+
+sumNumbers = 0
+for i in range(1, xInt):
+    sumNumbers += i
+
+print("Сумма", sumNumbers)
+
+sumNumbers = 0
+i = 1
+while i < xInt:
+    sumNumbers += i
+    i += 1
+
+print("Сумма", sumNumbers)
 """
